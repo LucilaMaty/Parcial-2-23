96ae621d442f84534d2abb67410da4a930750129
Lucila <400712@sistermas.frc.utn.edu.ar>
2026-06-09 18:28:55 -0300

    ajuste archivo de pedidios.services

--- backend/services/pedidos.services.js
+++ backend/services/pedidos.services.js
@@ -1,4 +1,4 @@
 import Pedido from '../models/pedido.js';
 import Menu from '../models/menu.js';
-import HistorialPedido from '../models/historial.js';
+import HistorialPedido from '../models/historialPedidos.js';
 import { Op } from 'sequelize';
